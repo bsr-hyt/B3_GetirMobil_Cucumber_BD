34cbd5c490d24dd45f1018fa95648eca96244307
busra <busrahyt42@gmail.com>
2023-05-29 02:08:56 +0300

    CukesRunner class has been created

---
+++ src/test/java/com/getir/runners/CukesRunner.java
@@ -0,0 +1,21 @@
+package com.getir.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports"
+        },
+        features = "src/test/resources/features",
+        glue = "com/getir/step_definitions",
+        dryRun = false,
+        tags = "@wip",
+        strict = true
+)
+public class CukesRunner {
+}
